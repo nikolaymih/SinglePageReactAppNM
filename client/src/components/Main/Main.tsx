97f0ba914bd96482ea4e96f84da9a9cf2d2f3dfa
Nikolay Mihaylov <n.mihaylov@abv.bg>
2021-11-24 12:53:08 +0200

    add bar chart  with reference values

--- client/src/components/Main/Main.tsx
+++ client/src/components/Main/Main.tsx
@@ -1,6 +1,29 @@
+import { useState, useEffect } from 'react';
+import { BarChart, Bar, XAxis, Cell, ResponsiveContainer, LabelList } from 'recharts';
+import { barColors, CustomizedAxisTick, data } from '../../constants/main.constants';
+import { IUser } from '../../interfaces/user.interface';
+import { getUserDataService } from '../../service/user.service';
+
 import './Main.css';
 
 const Main = () => {
+    // let [data, setData] = useState<IUser>({
+    //     _id: '',
+    //     name: '',
+    //     image: '',
+    //     userText: '',
+    //     userNumber: 0
+    // })
+
+    // useEffect(() => {
+    //     async function fetchUser() {
+    //         const userData = await getUserDataService();
+
+    //         setData(userData);
+    //     }
+    //     fetchUser();
+    // }, [data])
+
     return (
         <>
             <section className="referenceValues">
@@ -9,7 +32,25 @@ const Main = () => {
                 </p>
                 <div className="overallVurnerability">
                     <article className="graphValues">
-                        <p>article1</p>
+                        <ResponsiveContainer width="100%" height="100%">
+                            <BarChart
+                                width={400}
+                                height={250}
+                                data={data}
+                                barCategoryGap={2}
+                                margin={{ top: 15, right: 30, left: 20, bottom: 5 }}
+                            >
+                                <XAxis dataKey="name"  axisLine={false} width={150} tick={<CustomizedAxisTick />} tickSize={-1} />
+                                <Bar dataKey="pv" fill="#8884d8" radius={[10, 10, 0, 0]}>
+                                    <LabelList dataKey="pv" position="top" fill="black" />
+                                    {
+                                        data.map((entry, index) => (
+                                            <Cell key={`cell-${index}`} fill={barColors[index % 20]} />
+                                        ))
+                                    }
+                                </Bar>
+                            </BarChart>
+                        </ResponsiveContainer>
                     </article>
                     <article className="userResult">
                         <p>article2</p>
@@ -24,4 +65,3 @@ const Main = () => {
 }
 
 export default Main;
-
---
+++ client/src/constants/main.constants.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+export class CustomizedAxisTick extends React.Component {
+    render() {
+        const { x, y, payload } = this.props as any;
+        let firstWord: string = '';
+        let rest: string[] = [];
+
+        if (payload.value.length > 8) {
+            [firstWord, ...rest] = payload.value.split(' ');
+        } else {
+            firstWord = payload.value;
+        }
+
+        return (
+            <g transform={`translate(${x},${y})`}>
+                <text x={0} y={0} dy={12} fill="#666">
+                    <tspan textAnchor="middle" x="0">{firstWord}</tspan>
+                    <tspan textAnchor="middle" x="1" dy="20">{rest.join(' ')}</tspan>
+                    {/* <tspan textAnchor="middle" x="1" dy="20">{payload.value}</tspan> */}
+                </text>
+            </g>
+        );
+    }
+};
+
+export const data = [
+    {
+        name: 'Low Risk Business',
+        uv: 4000,
+        pv: 910,
+        amt: 2400,
+    },
+    {
+        name: 'Average Business',
+        uv: 3000,
+        pv: 645,
+        amt: 2210,
+    },
+    {
+        name: 'NC IDEA',
+        uv: 2000,
+        pv: 555,
+        amt: 2290,
+    }
+];
+
+export const barColors = ["#6CE5E8", "#42B8D5", "#2E3192"]
